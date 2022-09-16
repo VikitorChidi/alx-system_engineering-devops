aac8a2464e84f9277f21b8919e4a823a99d7513b
Victor <victorblessed9@gmail.com>
2022-09-16 16:57:43 -0500

    new file

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,10 @@
+# Kills a process name killmenow
+
+exec { 'pkill killmenow':
+  path     => '/usr/bin',
+  command  => 'pkill killmenow',
+  provider => shell,
+  returns  => [0, 1]
+}
+Footer
+© 2022 Gi
