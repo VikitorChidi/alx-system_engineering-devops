f95e1b04800d271d1bd517d717806cccf37bae85
Victor <victorblessed9@gmail.com>
2022-11-01 15:42:25 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,22 @@ the number of subscribers for a given subreddit.
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
     """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    header = {'User-Agent': 'CustomClient/1.0'}
-    req = requests.get(url, headers=header, allow_redirects=False)
-    if req.status-code != 200:
-        return (0)
-    req = req.json()
-    if "data" in req:
-        return (req.get("data").get("subscribers"))
-    else:
-        return (0)
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
